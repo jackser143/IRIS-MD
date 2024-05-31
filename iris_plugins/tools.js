372a24655ed193683f1631576b7474f4a4363b0e
jackser143 <139695706+jackser143@users.noreply.github.com>
2024-05-31 06:16:10 +0530

    Update tools.js

--- iris_plugins/tools.js
+++ iris_plugins/tools.js
@@ -4,22 +4,22 @@
 //                                                                                                      //
 //                                             𝚅.1.2.7                                                  // 
 //                                                                                                      //
-//                          ██╗ ██████╗ ██╗███████╗    ███╗   ███╗ ██████╗                              //
-//                          ██║ ██╔══██╗██║██╔════╝    ████╗ ████║ ██╔══██╗                             //
-//                          ██║ ██████╔╝██║███████╗    ██╔████╔██║ ██║  ██║                             //
-//                          ██║ ██╔══██╗██║╚════██║    ██║╚██╔╝██║ ██║  ██║                             //
-//                          ██║ ██║  ██║██║███████║    ██║ ╚═╝ ██║ ██████╔╝                             //
-//                          ╚═╝ ╚═╝  ╚═╝╚═╝╚══════╝    ╚═╝     ╚═╝ ╚═════╝                              //
+//                          
+
+Ą̷͈̰̘͓̖͍͙̫̬̗͔̺͓͇̐̈́M̴͎̝̝͔̺͉̺̮͎̜̐͂͘͝꙰̵̺͖͓͈̗̥̊̉̐̊̍͋̀̃͋̎͜͠M̸̳̪͎̤̫̘̋̇̂̍̈́͊͋̚꙰̸͙̭̼̤̦͍͈̻͖͒̑̑̂̔̎͗̑͘Ư̵̢̨̭̗̻̦̯͉̱̼͔͉̽̏́͒͆̓͗̅̎̃̈́̚͘͝꙰̶̫̪͖͔͍̻̟͓̣̗͓̰̹͒̑͜ͅ
+
+
+                              //
 //                                                                                                      //
-//                                          BY: VENOX-SENPAI                                            //
+//                                          BY: SYAM-SENPAI                                            //
 //                                                                                                      //
 //                                                                                                      //
 //══════════════════════════════════════════════════════════════════════════════════════════════════════//
 
 /**
 
-* @project_name : IRIS-MD
-* @Developer : Venox-Senpai
+* @project_name : AM꙰M꙰U꙰
+* @Developer : Syam-Senpai
 * @Version : v.1.2.7
 * @license : APACHE-2.0
 
@@ -29,7 +29,7 @@ TO A DMCA REPORT FROM THE OFFICIAL GITHUB, WHICH RESULT IN
 REMOVING UR COPIED/RE-UPLOADED PROJECT FROM THERE, IN SOME
 CONDITIONS IT CAN LEADS TO TERMINATE UR ACCOUNT FROM THAT PLATFORM
 
-│• @C_holder : Venox-Senpai
+│• @C_holder : Syam-Senpai
 
 **/
 
